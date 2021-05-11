72bee7f90f0adb72bba18c5cf07df1ac0bec5f12
ujaasb <50334456+ujaasb@users.noreply.github.com>
2021-05-11 14:02:45 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from mmpac import *
+
+# Load env variables 
+verticaUser = os.getenv('verticaUser')
+verticaPass = os.getenv('verticaPass')
+
+# Setup vertica connection
+print('Connecting to Vertica')
+vertica_setup(server='aws_prod',                                     
+              user=verticaUser,
+              password=verticaPass,
+              connection_timeout=3600)
+
+# List of sql files to run.
+# We could loop through all .sql files in the root folder, but this way we can manage the order of runs
+extract_list = [
+ 'cp_special_path_retention_temp.sql'
+]
+
+# Query and store all extracts from extract_dict
+for file_name in extract_list:
+    print('Running Query {sql}'.format(sql = file_name))
+    sql = open(file_name, 'r').read()
+    # Removing commented out DROP AND CREATE statements from sql
+    adjusted_sql = sql.replace('/*', '').replace('*/', '')
+    vsql(cmd = adjusted_sql)
